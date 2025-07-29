c55f74f908b050153d8e2ee624c8546570e0fac2
regenyi_akos <akos.regenyi@bluebax.com>
2025-07-29 11:50:07 +0200

    light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
+            <head>
+                <meta name='color-scheme' content='light' />
+            </head>
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 <Navigation links={LINKS1} />
                 {children}
